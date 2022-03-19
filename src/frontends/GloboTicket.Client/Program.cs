d6451d49e1fdf929daed8fd02b38684a25f7e632
Paulo Walraven <pmwc04@gmail.com>
2022-03-19 13:57:21 -0300

    clean start project and mock settings

--- src/frontends/GloboTicket.Client/Program.cs
+++ src/frontends/GloboTicket.Client/Program.cs
@@ -1,27 +1,24 @@
+using GloboTicket.Client.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var mvcBuilder = builder.Services.AddControllersWithViews();
+if (builder.Environment.IsDevelopment())
+    mvcBuilder.AddRazorRuntimeCompilation();
+
+builder.Services.AddSingleton<Settings>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
-
+"default",
+"{controller=Home}/{action=Index}/{id?}");
 app.Run();
